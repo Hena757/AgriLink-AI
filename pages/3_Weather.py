c6ed18c8650b3f362e709907d57b7e52784c4c2c
Hena757 <henapandit757@gmail.com>
2026-02-09 21:30:28 +0530

    Add weather forecast display with Streamlit

--- pages/3_Weather.py
+++ pages/3_Weather.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.title("🌦 Weather Forecast")
+
+st.metric("Temperature", "28°C")
+st.metric("Humidity", "78%")
+
+st.warning("🌧 Rain expected tomorrow")
+st.write("Avoid pesticide spraying")
 
