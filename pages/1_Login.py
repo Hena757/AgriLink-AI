64efdd3f24965a65d6052283830c1f749381da62
Hena757 <henapandit757@gmail.com>
2026-02-09 21:28:26 +0530

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+st.title("🔐 Login")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username:
+        st.session_state["logged_in"] = True
+        st.session_state["user"] = username
+        st.success("Login successful!")
+    else:
+        st.error("Please enter username")
 
